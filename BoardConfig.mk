a9b4ef3ada7f9ef770c89d9785e6b59d3a5ebd4e
DevelLevel <xda.homecook.xda@gmail.com>
2020-05-16 14:48:43 +0200

    Compile fixes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,5 +110,31 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/conf/thermal-engine.conf:system/etc/thermal-engine.conf
 
+#
+# This is for TWRP Recovery
+#
+
+# Path
+LOCAL_PATH := device/sony/suzuran
+TWRP_OUT := recovery/root
+
+# Copy kernel
+PRODUCT_COPY_FILES += device/sony/suzuran/kernel/Image.gz-dtb:kernel
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/twrp.fstab
+
 # TWRP
-$(call inherit-product, device/sony/suzuran/twrp.mk)
+TW_MAX_BRIGHTNESS := 255
+TW_EXCLUDE_SUPERSU := true
+TW_EXTRA_LANGUAGES := true
+TW_THEME := portrait_hdpi
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone25/temp"
+TWHAVE_SELINUX := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+
+PRODUCT_COPY_FILES += \
+    bionic/libc/zoneinfo/tzdata:$(TWRP_OUT)/system/usr/share/zoneinfo/tzdata
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/twrp.fstab:$(TWRP_OUT)/etc/twrp.fstab
