e3cfacee60f956ea634e39b2c2a03259a7e00d58
DevelLevel <xda.homecook.xda@gmail.com>
2021-03-11 12:26:03 +0000

    Messaging fix

--- device.mk
+++ device.mk
@@ -75,12 +75,16 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
 
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service
+
 # APN
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/../../../vendor/aosp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
+# Messaging
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    messaging
 
 # FDroid
 $(call inherit-product-if-exists, vendor/fdroid/fdroid-vendor.mk)
