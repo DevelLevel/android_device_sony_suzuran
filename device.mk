fc05c002a1156d7ab7f26cdf1731059c885ab5e3
DevelLevel <xda.homecook.xda@gmail.com>
2021-03-11 12:22:21 +0000

    APN fix

--- device.mk
+++ device.mk
@@ -75,6 +75,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
 
+# APN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/../../../vendor/aosp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service
 
