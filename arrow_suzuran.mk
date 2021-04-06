bbd31b54ab958183da251b6bcb7a09ff51114fb4
DevelLevel <xda.homecook.xda@gmail.com>
2021-04-06 13:09:10 +0000

    Fix typo

--- arrow_suzuran.mk
+++ arrow_suzuran.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, device/sony/suzuran/full_suzuran.mk)
 
 # Inherit some common LineageOS stuff
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # This indicates the first api level, device has been commercially launched on.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk)
